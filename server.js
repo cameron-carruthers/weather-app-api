7ed49b237d9f5ee3ec2661f35ebde4ca040742b6
Cameron Carruthers <cameronlcarruthers@gmail.com>
2021-01-27 11:41:47 -0700

    Add redis cache to prevent API overload

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const redis = require('redis');
 const { DateTime } = require('luxon');
 const axios = require('axios');
 const cors = require('cors')
 require('dotenv').config()
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const app = express();
+const client = redis.createClient(REDIS_PORT);
+
+const cache = (req, res, next) => {
+  const { city } = req.query;
+
+  client.get(city, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(JSON.parse(data));
+    } else {
+      next();
+    }
+  })
+}
 
 app.use(cors())
 
-app.get('/', async (req, res) => {
+app.get('/', cache, async (req, res) => {
 
   try {
     const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${req.query.city}&limit=1&appid=${process.env.API_KEY}`);
@@ -27,7 +45,10 @@ app.get('/', async (req, res) => {
       })
     });
 
-    res.send(arrayOfTemps.slice(0,5));
+    client.setex(req.query.city, 3600, JSON.stringify(arrayOfTemps.slice(0,5)), (err) => {
+      if (err) throw err;
+      res.send(arrayOfTemps.slice(0,5));
+    });
   } catch (err) {
     console.error(err);
   }
